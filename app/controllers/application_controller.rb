4b7eea74bfbe0178722256b8b91c0c2b6d430bac
Danilo Carvalho <danilogga@gmail.com>
2019-02-26 17:40:48 -0400

    start authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= User.find(username: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must be logged to perform this action"
+      redirect_to root_path
+    end
+  end
 end
