6aaa48c4c8a5e9ec499e57b465cd75f28450895c
Danilo Carvalho <danilogga@gmail.com>
2019-03-02 16:57:11 -0400

    config action cable with redis to go by heroku blog

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.disable_request_forgery_protection = true
-  #config.action_cable.allowed_request_origins = ['https://message-me-danilogga.herokuapp.com']
 end
